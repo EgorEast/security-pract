b7808c7fbce873e4babd901b9c58cbf210eabc3f
egoreast <egoreast.true@gmail.com>
2025-01-21 23:28:15 +0300

    add progress and fix styles

--- src/pages/player/ui/PlayerPage.tsx
+++ src/pages/player/ui/PlayerPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import useSound from 'use-sound' //для работы со звуком
 import youAreBeautiful from "./MercyMe - You're Beautiful.mp3" // импорт музыки
 import cover from './cover.jpeg'
@@ -10,8 +10,34 @@ import style from './style.module.css'
 
 export const PlayerPage = () => {
   const [isPlaying, setIsPlaying] = useState(false)
+  const [currTime, setCurrTime] = useState<{
+    min: number | null
+    sec: number | null
+  }>({
+    min: null,
+    sec: null,
+  }) // текущее положение звука в минутах и секундах
+
+  const [seconds, setSeconds] = useState() // текущая позиция звука в секундах
+
   const [play, { pause, duration, sound }] = useSound(youAreBeautiful)
-  console.log('sound: ', sound)
+
+  const time = useMemo(() => {
+    if (!duration)
+      return {
+        min: null,
+        sec: null,
+      }
+
+    const sec = duration / 1000
+    const min = Math.floor(sec / 60)
+    const secRemain = Math.floor(sec % 60)
+
+    return {
+      min: min,
+      sec: secRemain,
+    }
+  }, [duration])
 
   const playingButton = () => {
     if (isPlaying) {
@@ -23,9 +49,24 @@ export const PlayerPage = () => {
     }
   }
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([])) // устанавливаем состояние с текущим значением в секундах
+        const min = Math.floor(sound.seek([]) / 60)
+        const sec = Math.floor(sound.seek([]) % 60)
+        setCurrTime({
+          min,
+          sec,
+        })
+      }
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [sound])
+
   return (
     <div className={style.component}>
-      <h2>Playing Now</h2>
+      <h2 style={{ marginTop: 0 }}>Playing Now</h2>
       <img className={style.musicCover} src={cover} />
       <div>
         <h3 className={style.title}>MercyMe</h3>
@@ -55,6 +96,25 @@ export const PlayerPage = () => {
             <BiSkipNext />
           </IconContext.Provider>
         </button>
+      </div>
+      <div className={style.timelineContainer}>
+        <div className={style.time}>
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
+        </div>
+        <input
+          type='range'
+          min='0'
+          max={duration ? duration / 1000 : 100}
+          defaultValue='0'
+          value={seconds}
+          className={style.timeline}
+          onChange={e => sound.seek([e.target.value])}
+        />
       </div>
     </div>
   )
